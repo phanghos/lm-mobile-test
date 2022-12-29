89f4ec9b2f876b65445212c3d1292538c3c445c2
Roberto Tatasciore <tatasciorecont@gmail.com>
2022-12-29 11:50:16 +0100

    Render hotel list

--- src/components/HotelList/HotelList.tsx
+++ src/components/HotelList/HotelList.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  Insets,
+  ListRenderItem,
+  StyleSheet,
+  View,
+} from 'react-native';
 import type { Hotel } from 'types';
 import useFetchHotels from 'hooks/useFetchHotels';
+import HotelListItem from 'components/HotelListItem/HotelListItem';
+
+const renderItem: ListRenderItem<Hotel> = ({ item }) => (
+  <HotelListItem {...item} />
+);
 
 const keyExtractor = ({ id }: Hotel) => `${id}`;
+
+const Separator = () => <View style={styles.separator} />;
+
+const scrollInsets: Insets = { top: 16, bottom: -16 };
 
 const HotelList = () => {
   const { isLoading, hotels, hasError } = useFetchHotels();
@@ -14,8 +30,11 @@ const HotelList = () => {
       {!!hotels.length && (
         <FlatList
           data={hotels}
-          renderItem={({ item: _ }) => <></>}
+          renderItem={renderItem}
           keyExtractor={keyExtractor}
+          ItemSeparatorComponent={Separator}
+          contentContainerStyle={styles.listContainer}
+          scrollIndicatorInsets={scrollInsets}
         />
       )}
       {hasError && <></>}
@@ -27,4 +46,6 @@ export default HotelList;
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#F8F1FF' },
+  separator: { height: 8 },
+  listContainer: { padding: 16 },
 });
