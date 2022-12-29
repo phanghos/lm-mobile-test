bc3dc5165ee84454f522536d017b411ed18ea395
Roberto Tatasciore <tatasciorecont@gmail.com>
2022-12-29 10:15:19 +0100

    Add typings for entities and API responses and hook for fetching hotels

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export interface Hotel {
+  id: number;
+  name: string;
+  location: {
+    address: string;
+    city: string;
+    latitude: number;
+    longitude: number;
+  };
+  stars: number;
+  checkIn: { from: string; to: string };
+  checkOut: { from: string; to: string };
+  contact: {
+    phoneNumber: string;
+    email: string;
+  };
+  gallery: string[];
+  userRating: number;
+  price: number;
+  currency: string;
+}
+
+export type HotelResponse = Hotel[];
